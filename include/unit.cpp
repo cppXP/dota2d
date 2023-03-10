52f1fb199fc9525584bb225b81a769c3ffbb621a
mrx <albin@senioradministrator.com>
2023-03-10 13:04:53 +0100

    unit added but not fully.

---
+++ include/unit.cpp
@@ -0,0 +1,43 @@
+#include "unit.h"
+
+
+dota2d::Unit::Unit()
+{
+  setName("");
+  setSpawn_position(0.0 ,0.0);
+}
+
+void dota2d::Unit::setName(std::string _value)
+{
+  m_name = _value;
+}
+
+void dota2d::Unit::setSpawn_position(sf::Vector2f _pos)
+{
+  m_spawn_position = _pos;
+}
+
+void dota2d::Unit::setSpawn_position(float _x,float _y)
+{
+  m_spawn_position.x = _x;
+  m_spawn_position.y = _y;
+}
+
+std::string dota2d::Unit::getName() const
+{
+  return m_name;
+}
+
+sf::Vector2f dota2d::Unit::getSpawn_position() const
+{
+  return m_spawn_position;
+}
+
+//action
+void dota2d::Unit::printUnit_stats() const
+{
+  cout << "\n Unit::printUnit_stats : "
+       << "\n name = " << getName()
+       << "\n spawn-position = x: " << getSpawn_position().x << " y: " << getSpawn_position().y
+       << std::endl;
+}
