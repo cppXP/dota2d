bd3d21413d80fbf0519409067b0bbf76b1385d74
mrx <albin@senioradministrator.com>
2023-03-19 00:08:45 +0100

    constractors modifations

--- include/game.h
+++ include/game.h
@@ -5,23 +5,28 @@
 #include <string>
 #include "baseCamera.h"
 #include "baseWindow.h"
+#include "baseSprite.h"
 
 namespace dota2d
 {
-  class Game : public BaseWindow, public BaseCamera
+  class Game : public BaseWindow, public BaseCamera, public BaseSprite
   {
     public:
       Game(sf::Vector2i,sf::Vector2i,
            int w_height ,int w_width ,std::string w_title ,sf::Style w_style, //window
-           sf::Vector2i c_pos ,float c_zoom,float c_rotate,int c_speed,int c_mouseBorder, sf::FloatRect c_viewport// Camera
+           sf::Vector2i c_pos ,float c_zoom,float c_rotate,int c_speed,int c_mouseBorder, sf::FloatRect c_viewport, // Camera
+           std::string g_texture, sf::Vector2f g_spritePos //baseSprite
          ) : BaseWindow(w_height,w_width,w_title,w_style)
-           : BaseCamera(c_pos, c_zoom, c_rotate, c_speed, c_mouseBorder, c_viewport);
+           : BaseCamera(c_pos, c_zoom, c_rotate, c_speed, c_mouseBorder, c_viewport)
+           : BaseSprite(g_texture, g_spritePos);
 
       Game(sf::Vector2i,sf::Vector2i,
           int w_height ,int w_width ,std::string w_title ,sf::Style w_style, sf::Vector2i w_pos, //window
-          sf::Vector2i c_pos ,float c_zoom,float c_rotate,int c_speed,int c_mouseBorder, sf::FloatRect c_viewport// Camera
+          sf::Vector2i c_pos ,float c_zoom,float c_rotate,int c_speed,int c_mouseBorder, sf::FloatRect c_viewport, // Camera
+          std::string g_texture, sf::Vector2f g_spritePos //baseSprite
         ) : BaseWindow(w_height,w_width,w_title,w_style,w_pos)
-          : BaseCamera(c_pos, c_zoom, c_rotate, c_speed, c_mouseBorder, c_viewport);
+          : BaseCamera(c_pos, c_zoom, c_rotate, c_speed, c_mouseBorder, c_viewport)
+          : BaseSprite(g_texture, g_spritePos);
 
       void setMap_min_pos(sf::Vector2i);
       void setMap_max_pos(sf::Vector2i);
