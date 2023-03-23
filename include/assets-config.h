f4ddbb976adef19e9b307e72b41f344cd58365cb
mrx <albin@senioradministrator.com>
2023-03-23 20:57:27 +0100

    messages removed from config file

--- include/assets-config.h
+++ include/assets-config.h
@@ -2,15 +2,4 @@
 #define ASSEST_GAME_BACKGROUND_TEXTURE "./assets/game/game-background.jpg"
 
 
-
-
-
-
-
-
-
-#define MESSAGE_TEXT_TEXTURE_FAILED_TO_LOAD "can not load texture "
-#define MESSAGE_TEXT_TEXTURE_SUCCESS_LOADED "texture loaded successfully "
-
-#define MESSAGE_TEXT_TEXUTRE_DEFAULT_CANT_LOAD "can not load default texture "
-#define MESSAGE_TEXT_TEXTURE_DEFAULT_LOADED "default texture loaded "
+#define LOG_FILE_NAME "dota2d.log"
