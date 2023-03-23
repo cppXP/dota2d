b20b826bb4278f124a9d8848695f94e8132f6242
mrx <albin@senioradministrator.com>
2023-03-23 23:20:15 +0100

    window width and height value changed to desktop size.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
   dota2d::Game game
   (
     sf::Vector2i(0,1000),sf::Vector2i(0,1000), //map min&max
-    0,0, "Dota2d window.", //window
+    sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().height, "Dota2d window.", //window
     sf::Vector2i(0,0), 0.50, 0.0, 5, 5, sf::FloatRect(0,0, 1.0f, 1.0f), //camera
     "./assets/game/game-background.jpg" , sf::Vector2f(0.0, 0.0)
   );
