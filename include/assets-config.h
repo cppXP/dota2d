cbd490067cf65a66dbaca31d3e7ddc93a3d38da9
mrx <albin@senioradministrator.com>
2023-03-24 17:06:00 +0100

    assets added.

--- include/assets-config.h
+++ include/assets-config.h
@@ -1,5 +1,8 @@
 #define ASSEST_DEFAULT_TEXTURE "./assets/default-texture.jpg"
-#define ASSEST_GAME_BACKGROUND_TEXTURE "./assets/game/game-background.jpg"
+#define ASSEST_GAME_BACKGROUND_TEXTURE_1 "./assets/game/game-background.jpg"
+#define ASSEST_GAME_BACKGROUND_TEXTURE_2 "./assets/game/tem-map.png"
+
+
 
 
 #define LOG_FILE_NAME "dota2d-log"
