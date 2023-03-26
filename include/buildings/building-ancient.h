a08ad658dad85e71adb5f96bb091083da15e3a21
mrx <albin@senioradministrator.com>
2023-03-26 20:22:23 +0200

    constractor modified, comiled fine, healhBar shows but need to work (how to position on top-center object)

--- include/buildings/building-ancient.h
+++ include/buildings/building-ancient.h
@@ -10,7 +10,7 @@ namespace dota2d
       Ancient(int _hp ,int _max_hp ,float _hp_regen ,float _physical_rasistant ,float _magical_rasistant,
                             TeamSide _side,std::string _texture,sf::Vector2f _pos
               ) : BuildingAttackable(_hp, _max_hp, _hp_regen, _physical_rasistant,
-                            _magical_rasistant,_side, _texture, _pos)
+                            _magical_rasistant, sf::Vector2f(_pos.x-25 ,_pos.y-25) ,_side, _texture, _pos)
                             {
 
                             }
--- include/buildings/buildingAttackable.h
+++ include/buildings/buildingAttackable.h
@@ -9,10 +9,11 @@ namespace dota2d
   class BuildingAttackable : public BaseBuilding , public BaseHealth
   {
     public:
-      BuildingAttackable(int _hp ,int _max_hp ,float _hp_regen ,float _physical_rasistant ,float _magical_rasistant, //baseHealth
+      BuildingAttackable(int _hp ,int _max_hp ,float _hp_regen ,float _physical_rasistant, //baseHealth
+            float _magical_rasistant, sf::Vector2f _healthBar_pos, //baseHealth
             TeamSide _side, std::string _texture ,sf::Vector2f _pos //baseBuilding
           ) : BaseBuilding(_side, _texture, _pos)
-            , BaseHealth(_hp, _max_hp, _hp_regen, _physical_rasistant, _magical_rasistant)
+            , BaseHealth(_hp, _max_hp, _hp_regen, _physical_rasistant, _magical_rasistant, _healthBar_pos)
             {
 
             }
