4cb8ef10d8355c8056de1e0d942c28d7a5d9b615
mrx <albin@senioradministrator.com>
2023-03-26 19:24:45 +0200

    problem bad texutre sovled.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
   dota2d::Game game
   (
     sf::Vector2i(0,0),sf::Vector2i(2000,2000), //map min&max
-    sf::VideoMode::getDesktopMode().width,sf::VideoMode::getDesktopMode().height, "Dota2d window.", //window
+    1024,1024, "Dota2d window.", //window sf::VideoMode::getDesktopMode().width sf::VideoMode::getDesktopMode().height
     sf::Vector2i(0,0), 0.50, 0.0, 5, 300, sf::FloatRect(0,0, 1.0f, 1.0f), //camera
     ASSEST_GAME_BACKGROUND_TEXTURE_1 , sf::Vector2f(-50, -50)
   );
