1a87d53a89f948dd0e1a2fff4417e74590ab8263
mrx <albin@senioradministrator.com>
2023-03-26 19:52:35 +0200

    asset added.

--- include/assets-config.h
+++ include/assets-config.h
@@ -3,6 +3,6 @@
 #define ASSEST_GAME_BACKGROUND_TEXTURE_2 "./assets/game/tem-map.png"
 
 #define ASSEST_BUILDING_SHRINE "./assets/buildings/shrine.png"
-
+#define ASSEST_BUILDING_ANCIENT "./assets/buildings/ancient.png"
 
 #define LOG_FILE_NAME "dota2d-log"
