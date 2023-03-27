42d0eb9481e4816881304fb334aed0a6648d160a
mrx <albin@senioradministrator.com>
2023-03-27 16:48:58 +0330

    tring to make window better. failed

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
   dota2d::Game game
   (
     sf::Vector2i(0,0),sf::Vector2i(2000,2000), //map min&max
-    1024,1024, "Dota2d window.", //window sf::VideoMode::getDesktopMode().width sf::VideoMode::getDesktopMode().height
+    800, 600, "Dota2d window.", //window sf::VideoMode::getDesktopMode().width*1.50 sf::VideoMode::getDesktopMode().height*1.50
     sf::Vector2i(0,0), 0.50, 0.0, 5, 300, sf::FloatRect(0,0, 1.0f, 1.0f), //camera
     ASSEST_GAME_BACKGROUND_TEXTURE_1 , sf::Vector2f(-50, -50)
   );
