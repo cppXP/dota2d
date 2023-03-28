9a87f4bbf78bb1d188d086e812641639f11c393e
mrx <albin@senioradministrator.com>
2023-03-29 01:53:16 +0330

    assest added.

--- include/assets-config.h
+++ include/assets-config.h
@@ -2,7 +2,32 @@
 #define ASSEST_GAME_BACKGROUND_TEXTURE_1 "./assets/game/game-background.jpg"
 #define ASSEST_GAME_BACKGROUND_TEXTURE_2 "./assets/game/tem-map.png"
 
-#define ASSEST_BUILDING_SHRINE "./assets/buildings/shrine.png"
-#define ASSEST_BUILDING_ANCIENT "./assets/buildings/ancient.png"
 
 #define LOG_FILE_NAME "dota2d-log"
+
+
+#define ASSEST_BUILDING_FOUNTAIN_DIRE "./assets/buildings/fountain-dire.png"
+#define ASSEST_BUILDING_FOUNTAIN_RADIANT "./assets/buildings/fountain-radiant.png"
+
+#define ASSEST_BUILDING_ANCIENT_DIRE "./assets/buildings/ancient-dire.png"
+#define ASSEST_BUILDING_ANCIENT_RADIANT "./assets/buildings/ancient-radiant.png"
+
+#define ASSEST_BUILDING_SECRETSHOP "./assets/buildings/secretshop.png"
+
+#define ASSEST_BUILDING_OUTPOST_DIRE "./assets/buildings/outpost-dire.png"
+#define ASSEST_BUILDING_OUTPOST_RADIANT "./assets/buildings/outpost-radiant.png"
+
+#define ASSEST_BUILDING_SHRINE_DIRE "./assets/buildings/shrine-dire.png"
+#define ASSEST_BUILDING_SHRINE_RADIANT "./assets/buildings/shrine-radiant.png"
+
+#define ASSEST_BUILDING_PILLAR_DIRE "./assets/buildings/pillar-dire.png"
+#define ASSEST_BUILDING_PILLAR_RADIANT "./assets/buildings/pillar-radiant.png"
+
+#define ASSEST_BUILDING_MELEE_BARRACKS_DIRE "./assets/buildings/barracks-melee-dire.png"
+#define ASSEST_BUILDING_MELEE_BARRACKS_RADIANT "./assets/buildings/barracks-melee-radiant.png"
+
+#define ASSEST_BUILDING_RANGED_BARRACKS_DIRE "./assets/buildings/barracks-ranged-dire.png"
+#define ASSEST_BUILDING_RANGED_BARRACKS_RADIANT "./assets/buildings/barracks-ranged-radiant.png"
+
+#define ASSEST_BUILDING_RANGED_TOWER_DIRE "./assets/buildings/tower-dire.png"
+#define ASSEST_BUILDING_RANGED_TOWER_RADIANT "./assets/buildings/tower-radiant.png"
