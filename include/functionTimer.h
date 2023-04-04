e790b24b71e87a39f84d33454266bd87c2cfe816
mrx <albin@senioradministrator.com>
2023-04-04 22:58:34 +0330

    tried to add baseLog and write things as log but can not convert autoDuration and others to string, work later on it those codes commented.

--- include/functionTimer.h
+++ include/functionTimer.h
@@ -3,10 +3,12 @@
 #include <chrono>
 #include <iostream>
 #include <string>
+// #include "baseLog.h"
 
 /*
-  Code from:
-    https://stackoverflow.com/questions/58487924/using-chrono-for-time-measurement
+    Thanks to The Cherno Youtube channle
+    AND
+    SourceCode: https://stackoverflow.com/questions/58487924/using-chrono-for-time-measurement
 */
 
 namespace dota2d
@@ -16,6 +18,7 @@ namespace dota2d
       private:
           std::chrono::time_point<std::chrono::high_resolution_clock> pr_StartTime, pr_EndTime;
           std::string function_name;
+          // BaseLog m_log;
 
       public:
           void init(std::string _text)
@@ -23,6 +26,7 @@ namespace dota2d
             pr_StartTime = std::chrono::high_resolution_clock::now();
             function_name = _text;
             std::cout << "function " << function_name << " has been starts.\n";
+            // m_log.debug("function " << function_name << " has been starts.\n");
           }
           void finish()
           {
@@ -32,6 +36,7 @@ namespace dota2d
             auto EndTimeMs = std::chrono::time_point_cast<std::chrono::microseconds>(pr_EndTime).time_since_epoch().count();
             auto Duration = EndTimeMs - StartTimeMs;
             std::cout << "function " << function_name<< " took " << Duration << "ms to finish.\n";
+            // m_log.debug("function " << function_name<< " took " << Duration << "ms to finish.\n");
           }
 
     };
