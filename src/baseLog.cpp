e63588bb58cfc624cc7bcdbd838999437c941e85
mrx <albin@senioradministrator.com>
2023-04-06 21:27:14 +0330

    log file was larg, so commented for now to dont add to that file.

--- src/baseLog.cpp
+++ src/baseLog.cpp
@@ -27,25 +27,25 @@ void dota2d::BaseLog::set_post_message(std::string _text)
 
 void dota2d::BaseLog::fatal(std::string _text)
 {
-  PLOG(plog::fatal) << m_pre_message + _text + m_post_message;
+  // PLOG(plog::fatal) << m_pre_message + _text + m_post_message;
 }
 
 void dota2d::BaseLog::error(std::string _text)
 {
-  PLOG(plog::error) << m_pre_message + _text + m_post_message;
+  // PLOG(plog::error) << m_pre_message + _text + m_post_message;
 }
 
 void dota2d::BaseLog::warning(std::string _text)
 {
-  PLOG(plog::warning) << m_pre_message + _text + m_post_message;
+  // PLOG(plog::warning) << m_pre_message + _text + m_post_message;
 }
 
 void dota2d::BaseLog::info(std::string _text)
 {
-  PLOG(plog::info) << m_pre_message + _text + m_post_message;
+  // PLOG(plog::info) << m_pre_message + _text + m_post_message;
 }
 
 void dota2d::BaseLog::debug(std::string _text)
 {
-  PLOG(plog::debug) << m_pre_message + _text + m_post_message;
+  // PLOG(plog::debug) << m_pre_message + _text + m_post_message;
 }
