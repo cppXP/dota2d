91d071b3faf2084c81140db851431ec7f8c9090a
mrx <albin@senioradministrator.com>
2023-04-06 15:10:46 +0330

    scripts moved into directory

---
+++ scripts/gpp-compile.sh
@@ -0,0 +1,1 @@
+g++ main.cpp ./include/*.h ./include/buildings/*.h ./src/*.cpp -o dota2d.run -lsfml-graphics -lsfml-window -lsfml-system -lsfml-network
---
+++ scripts/library-plog-export.sh
@@ -0,0 +1,3 @@
+#export CPLUS_INCLUDE_PATH=/home/mrx/Downloads/sfml/include
+export CPLUS_INCLUDE_PATH=/home/mrx/Downloads/plog-master/include/
+
